f37ebd852d764ffc18d9461d5076547409e3894c
SuperShika <66006320+SuperShika@users.noreply.github.com>
2023-05-03 12:46:27 +0300

    R3

---
+++ SuprafataSectoare.py
@@ -0,0 +1,10 @@
+#Selecteaza Sectoarele
+arcpy.SelectLayerByAttribute_management("Sector_Cadastral","NEW_SELECTION",""" "Nr_SC" In (13,17,2,9) """)
+#reseteaza suma suprafetelor
+sum = 0.0
+for row in arcpy.SearchCursor("Sector_Cadastral"):
+    sum = sup+row.Shape_Area
+      
+#arata rezultat
+print sum
+#nu uita sa deselectezi daca mai lucrezi cu layerul asta
